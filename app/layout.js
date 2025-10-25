ba1e9411ee659ae01aa6a537faa7f7bf2114b3fe
Harshitgupta2915 <9charshitgupta@gmail.com>
2025-10-25 19:00:32 +0530

    views

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 const inter = Inter({ subsets: ["latin"] });
 import Nav from "../components/Nav";
 export default function RootLayout({ children }) {
@@ -8,7 +9,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="[scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden">
       <body>
-           <Nav></Nav>
+        <Nav></Nav>
+        <Analytics />
         {children}
       
       </body>
